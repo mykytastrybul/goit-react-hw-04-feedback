1c4b6d0d9f929d7cd7dd3ed895d6f91924bc69b3
s3bull <stribla@yahoo.com>
2022-05-06 16:23:03 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,15 +5,14 @@ import Section from './Section/Section';
 import Notification from './Notification/Notification';
 
 export function App() {
-
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   const addFeedback = key => {
-    if (key === 'good') return setGood(good + 1);
-    if (key === 'neutral') return setNeutral(neutral + 1);
-    if (key === 'bad') return setBad(bad + 1);
+    if (key === 'good') return setGood(prev => prev + 1);
+    if (key === 'neutral') return setNeutral(prev => prev + 1);
+    if (key === 'bad') return setBad(prev => prev + 1);
   };
 
   const countTotalFeedback = good + neutral + bad;
